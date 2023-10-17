cf0fa0b85abeaa9441ecad5d3ee64d0f5f2a0abf
kosyo1204 <kosyo1204@users.noreply.github.com>
2023-10-17 22:04:32 +0900

    inter raction

--- index.js
+++ index.js
@@ -20,10 +20,11 @@
 // */
 
 // データ型
-let name = "Ilya";
-
+// let name = "Ilya";
 // alert( `hello ${1}` ); // hello1
-
-alert( `hello ${`name`}` ); // hello "name"
-
+// alert( `hello ${`name`}` ); // hello "name"
 // alert( `hello ${name}` ); // hello Ilya
+
+// インタラクティブ
+const answer = prompt('What is your name?', '');
+alert(answer);
