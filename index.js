e5708b786295ea14533af46981a41eebc00835c9
kosyo1204 <kosyo1204@users.noreply.github.com>
2023-10-21 08:59:50 +0900

    operand

--- index.js
+++ index.js
@@ -30,32 +30,42 @@
 // alert(answer);
 
 // 演算子
-let a = 1, b = 1;
-let c = ++a;
-let d = b++;
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
 // a=2,b=2,c=2,d=1
 
-let a = 2;
-let x = 1 + (a *= 2);
+// let a = 2;
+// let x = 1 + (a *= 2);
 // a=4, x=5
 
-"" + 1 + 0 //10
-"" - 1 + 0 //-1
-true + false //1  //
-6 / "3" //2
-"2" * "3" //6
-4 + 5 + "px" //9px
-"$" + 4 + 5 //$45
-"4" - 2 //2
-"4px" - 2 //NaN
-7 / 0 //infinity
-"  -9  " + 5 //  -9  5
-"  -9  " - 5 // -14
-null + 1 // 1
-undefined + 1 // NaN
-" \t \n" - 2 //NaN // ->-2
+// "" + 1 + 0 //10
+// "" - 1 + 0 //-1
+// true + false //1  //
+// 6 / "3" //2
+// "2" * "3" //6
+// 4 + 5 + "px" //9px
+// "$" + 4 + 5 //$45
+// "4" - 2 //2
+// "4px" - 2 //NaN
+// 7 / 0 //infinity
+// "  -9  " + 5 //  -9  5
+// "  -9  " - 5 // -14
+// null + 1 // 1
+// undefined + 1 // NaN
+// " \t \n" - 2 //NaN // ->-2
 
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
-alert(+a + +b); // 12
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+// alert(+a + +b); // 12
 // promptによって返却される値の型が、文字列であるため。
+
+// 比較
+// 5 > 4 true
+// "apple" > "pineapple" false
+// "2" > "12" true 文字列同士なので辞書の比較を行う
+// undefined == null true
+// undefined === null false
+// null == "\n0\n" false
+// null === +"\n0\n" false
+
